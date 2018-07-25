c2c7bbb8919344109a053adcd3f55c964fa42584
antonyadhiban <gtadhib@gmail.com>
2018-07-25 16:01:40 +0530

    Move products to main url

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('products', views.ProductListView.as_view()),
+    url('', views.ProductListView.as_view()),
     url('api/product', views.ProductListCreate.as_view()),
 ]
